8fec9add822503ea921b3e2fc6fd67690f28d21a
nostraJello <angeloufuentes751@gmail.com>
2024-05-26 03:52:04 +0800

    back buttons

--- java/com/intprog/farmfund/activities/SettingsActivity.kt
+++ java/com/intprog/farmfund/activities/SettingsActivity.kt
@@ -2,11 +2,18 @@ package com.intprog.farmfund.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.intprog.farmfund.R
 
 class SettingsActivity : AppCompatActivity() {
+    private lateinit var backButton: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        backButton = findViewById<ImageButton>(R.id.backButton)
+
+        backButton.setOnClickListener {
+            finish()
+        }
     }
 }
